d85ec1ea747220c43e00adbf3dca31be4f8169f5
TJ James <jamestw13@gmail.com>
2021-11-23 18:32:50 -0600

    began adding handlebars, created homepage template showing posts

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const {json} = require('body-parser');
 const {Post, User, Vote, Comment} = require('../../models');
 const {sequelize} = require('../../models/User');
 
